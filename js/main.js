19ced32e7514abf0ca47c70070ab4ed92a6dace0
ahmedrihin <ahmedrihin@gmail.com>
2021-07-13 20:42:55 +0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.main-slider').slick({
+        arrows: false,
+        dots: true,
+        dotsClass: 'dots'
+    })
+
+    $('.client-slider').owlCarousel({
+        items: 2,
+        
+    })
+
+
+})
